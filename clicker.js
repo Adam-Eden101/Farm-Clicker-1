1290eafc388034fee8beaabc47d8c33794e8ab77
STNevios <jealous91@outlook.com>
2016-11-24 23:22:48 +0100

    Add files via upload

--- clicker.js
+++ clicker.js
@@ -1,7 +1,12 @@
 i = 0;
+value_dps = 0;
+count_nami = 0;
+count_miss_fortune = 0;
 
 while (true)
-dps();
+{
+	dps();
+}
 
 function clicking() {
 	console.log(i);
@@ -17,8 +22,18 @@ function add() {
 	display_points();
 }
 
-function dps(){
+function dps() {
     i += 5;
     window.setTimeout('dps()', 1000);
     display_points();
+}
+
+function buy_nami() {
+	count_nami++;
+	document.getElementById('shop.count.nami').innerHTML = count_nami;
+}
+
+function buy_miss_fortune() {
+	count_miss_fortune++;
+	document.getElementById('shop.count.miss-fortune').innerHTML = count_miss_fortune;
 }
