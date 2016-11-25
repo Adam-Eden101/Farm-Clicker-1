acd1ded4fc95515a8c983c2ee5003d5d1bcaf3f8
STNevios <jealous91@outlook.com>
2016-11-25 09:46:47 +0100

    Shop opérationnel et divers affichages

--- clicker.js
+++ clicker.js
@@ -1,10 +1,16 @@
+//General
 i = 0;
-value_dps = 0;
-count_nami = 0;
-count_miss_fortune = 0;
-dps_nami = 0;
-dps_miss_fortune = 0;
 dps_total = 0;
+
+//Nami
+count_nami = 0;
+dps_nami = 0;
+prix_nami = 100;
+
+//Miss Fortune
+count_miss_fortune = 0;
+dps_miss_fortune = 0;
+prix_miss_fortune = 1000;
 
 while (true)
 {
@@ -31,16 +37,32 @@ function dps() {
 	dps_miss_fortune = count_miss_fortune * 5;
 	dps_total = dps_nami + dps_miss_fortune;
     i += dps_total;
+    if (dps_nami > 0)
+		document.getElementById('shop.dps.nami').innerHTML = dps_nami;
+    if (dps_miss_fortune > 0)
+		document.getElementById('shop.dps.miss-fortune').innerHTML = dps_miss_fortune;
     window.setTimeout('dps()', 1000);
     display_points();
 }
 
 function buy_nami() {
-	count_nami++;
-	document.getElementById('shop.count.nami').innerHTML = count_nami;
+	if (i >= prix_nami) {
+		i = i - prix_nami;
+		prix_nami = prix_nami + ((100 * count_nami)/10);
+		count_nami++;
+		document.getElementById('shop.price.nami').innerHTML = prix_nami;
+		document.getElementById('shop.count.nami').innerHTML = count_nami;
+	}
+	display_points();
 }
 
 function buy_miss_fortune() {
-	count_miss_fortune++;
-	document.getElementById('shop.count.miss-fortune').innerHTML = count_miss_fortune;
+	if (i >= prix_miss_fortune) {
+		i = i - prix_miss_fortune;		
+		prix_miss_fortune = prix_miss_fortune + ((100 * count_miss_fortune)/10);
+		count_miss_fortune++;
+		document.getElementById('shop.price.miss-fortune').innerHTML = prix_miss_fortune;
+		document.getElementById('shop.count.miss-fortune').innerHTML = count_miss_fortune;
+	}
+	display_points();
 }
